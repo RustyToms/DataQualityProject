d8ec666e088327232f2b380ebc152788102372a5
Jonathan Toms <jonathan.toms@kellogg.ox.ac.uk>
2024-04-20 13:34:48 -0600

    make intersection of unique and consistent datasets

--- scripts.py
+++ scripts.py
@@ -34,6 +34,9 @@ def save_validated_vulns(dataset, filepath, overwrite=False):
     print(f'{len(correct)} vulnerable samples written to {filepath} from {dataset}')
 
 def collect_all_validated_vulnerabilties(filepath):
+    """
+    Cycle through all known datasets collecting validated vulnerability samples, write to a csv file
+    """
     is_first = True
     for dataset in KNOWN_DATASETS:
         save_validated_vulns(dataset, filepath, is_first)
@@ -41,14 +44,27 @@ def collect_all_validated_vulnerabilties(filepath):
     file = pd.read_csv(filepath)
     print(f'Saved {len(file)} vulnerable samples to {filepath}')
 
-def export_to_jsonl(dataset_filepath, filepath, jsonl_structure):
+def intersection_consistent_unique(dataset, filepath):
+    consistent = pd.read_csv(f'results/consistent_datasets/{dataset}.csv')
+    unique = pd.read_csv(f'results/unique_datasets/{dataset}.csv')
+    intersection = pd.merge(consistent, unique, how='inner', on=['UID'])
+    intersection.dropna(inplace=True)
+    intersection.to_csv(filepath, mode='w', index=False, header=True)
+    
+    print(f'{len(consistent)} consistent samples found, {len(unique)} unique samples found, {len(intersection)} consistent and unique samples found, saved to {filepath}')
+
+def export_to_jsonl(dataset_filepath, jsonl_filepath, jsonl_structure):
+    """
+    Convert a csv dataset to a jsonl dataset, converting columns to keys as described in jsonl_structure
+    """
     data =  pd.read_csv(dataset_filepath)
     data2 = data[jsonl_structure.values()]
     # invert keys and values
     inv_structure = {v: k for k,v in jsonl_structure.items()}
     data2.rename(columns=inv_structure, inplace=True)
     print(data2)
-    data2.to_json(path_or_buf=filepath, orient='records', lines=True)
+    data2.to_json(path_or_buf=jsonl_filepath, orient='records', lines=True)
+    print(f'Exported {dataset_filepath} to {jsonl_filepath} in jsonl format, matching these keys to columns: {jsonl_structure}')
 
 
 
@@ -70,6 +86,7 @@ if __name__ == '__main__':
         'collect_all_validated_vulnerabilties': collect_all_validated_vulnerabilties,
         'save_validated_vulns': save_validated_vulns,
         'export_to_jsonl': export_to_jsonl,
+        'intersection_consistent_unique': intersection_consistent_unique,
     }
 
 
@@ -91,6 +108,12 @@ if __name__ == '__main__':
         if not bool(args.jsonl_structure):
             raise ValueError(f'--jsonl_structure must be included with {args.script}, as comma-separated json_key:pandas.DataFrame columns pairs, e.g. code:Function,idx:ID,status:Vulnerable')
         export_to_jsonl(args.dataset_filepath, args.output_filepath, args.jsonl_structure)
+    elif (args.script == 'intersection_consistent_unique'):
+        if args.output_filepath == "":
+            raise ValueError(f'--ouput_filepath must be included with {args.script}')
+        if args.dataset == "":
+            raise ValueError(f'--dataset must be included with {args.script}')
+        intersection_consistent_unique(args.dataset, args.output_filepath)
     else:
         raise ValueError(f'--script is {args.script}, but must be one of: {list(scripts.keys())}')
 
